6522926bc9a7b49ea1808e75592bcc42c4062754
alakhr <alakh@binmile.com>
2022-01-17 17:15:42 +0530

    show message

--- server.js
+++ server.js
@@ -9,25 +9,21 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-const Role = db.role;
-
-db.sequelize.sync();
+//const db = require("./app/models");
+//const Role = db.role;
+//db.sequelize.sync();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Alakh application." });
 });
 
-require('./app/routes/auth.routes')(app);
-require('./app/routes/movie.routes')(app);
+//require('./app/routes/auth.routes')(app);
+//require('./app/routes/movie.routes')(app);
 
-
-const PORT = 8000;
+const PORT = 8001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
